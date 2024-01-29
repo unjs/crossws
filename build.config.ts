f37f8e8ee1164f66a2f41a08db6e2b9fd139d9f0
Pooya Parsa <pooya@pi0.io>
2024-01-29 03:23:31 +0100

    build: generate backward compatible types

--- build.config.ts
+++ build.config.ts
@@ -1,3 +1,5 @@
+import { mkdir, writeFile } from "node:fs/promises";
+import { dirname, join } from "node:path";
 import { defineBuildConfig } from "unbuild";
 
 export default defineBuildConfig({
@@ -5,4 +7,24 @@ export default defineBuildConfig({
     inlineDependencies: true,
   },
   externals: ["@cloudflare/workers-types", "bun-types"],
+  hooks: {
+    async "build:done"(ctx) {
+      const entries = Object.keys(ctx.pkg.exports || {})
+        .filter((key) => key.startsWith("./"))
+        .map((key) => key.slice(2));
+      for (const entry of entries) {
+        const dst = join(ctx.options.rootDir, entry + ".d.ts");
+        console.log(">", dst);
+        await mkdir(dirname(dst), { recursive: true });
+
+        await writeFile(
+          dst,
+          `export * from "${
+            "..".repeat(entry.split("/").length - 1) || "."
+          }/dist/${entry}";\n`,
+          "utf8",
+        );
+      }
+    },
+  },
 });
