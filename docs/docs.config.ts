48f0caad64b8d24e40fd158495f2e4a75d77b4e6
Pooya Parsa <pooya@pi0.io>
2024-02-01 18:53:16 +0100

    docs: migrate to unjs-docs structure v2 (#13)
    * docs: migrate to unjs-docs structure v2
    
    * update to stable

--- docs/docs.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineDocsConfig } from "unjs-docs/config";
-
-export default defineDocsConfig({
-  name: "CrossWS",
-  description:
-    "Cross-platform WebSocket Servers for Node.js, Deno, Bun and Cloudflare Workers.",
-  github: "unjs/crossws",
-  themeColor: "#f7932a",
-});
