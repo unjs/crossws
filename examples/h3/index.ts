8ed7908e3d0db3e5e5755756a49a004af429fad7
Pooya Parsa <pooya@pi0.io>
2024-02-25 18:37:52 +0100

    update example with listhen 1.7.1

--- examples/h3/index.ts
+++ examples/h3/index.ts
@@ -6,23 +6,25 @@ export const app = createApp();
 // Listhen automatically sets up integration!
 // Learn more: https://crossws.unjs.io
 
-export const websocket = defineHooks({
-  open(peer) {
-    console.log("[ws] open", peer);
-  },
+export const websocket = {
+  hooks: defineHooks({
+    open(peer) {
+      console.log("[ws] open", peer);
+    },
 
-  message(peer, message) {
-    console.log("[ws] message", peer, message);
-    if (message.text().includes("ping")) {
-      peer.send("pong");
-    }
-  },
+    message(peer, message) {
+      console.log("[ws] message", peer, message);
+      if (message.text().includes("ping")) {
+        peer.send("pong");
+      }
+    },
 
-  close(peer, event) {
-    console.log("[ws] close", peer, event);
-  },
+    close(peer, event) {
+      console.log("[ws] close", peer, event);
+    },
 
-  error(peer, error) {
-    console.log("[ws] error", peer, error);
-  },
-});
+    error(peer, error) {
+      console.log("[ws] error", peer, error);
+    },
+  }),
+};
