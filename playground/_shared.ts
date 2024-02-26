a6879bd3208d09fce5365e7bfb293a7986795d29
Pooya Parsa <pooya@pi0.io>
2024-02-26 12:59:15 +0100

    chore: update playground

--- playground/_shared.ts
+++ playground/_shared.ts
@@ -33,6 +33,27 @@ export function createDemo<T extends Adapter<any, any>>(
     },
   });
 
+  // open(peer) {
+  //   // Send welcome to the new client
+  //   peer.send("Welcome to the server!");
+
+  //   // Join new client to the "chat" channel
+  //   peer.subscribe("chat");
+
+  //   // Notify every other connected client
+  //   peer.publish("chat", `[system] ${peer} joined!`);
+  // },
+
+  // message(peer, message) {
+  //   // The server re-broadcasts incoming messages to everyone
+  //   peer.publish("chat", `[${peer}] ${message}`);
+  // },
+
+  // close(peer) {
+  //   peer.publish("chat", `[system] ${peer} has left the chat!`);
+  //   peer.unsubscribe("chat");
+  // },
+
   const resolve: ResolveHooks = (info) => {
     return {
       open: (peer) => {
