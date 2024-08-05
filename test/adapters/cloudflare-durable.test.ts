a46265cec9ee3f88598eda545827c8f3c2495faa
Pooya Parsa <pooya@pi0.io>
2024-08-05 09:08:41 +0200

    test(cloudflare): use random port for wrangler inspector

--- test/adapters/cloudflare-durable.test.ts
+++ test/adapters/cloudflare-durable.test.ts
@@ -2,5 +2,7 @@ import { describe } from "vitest";
 import { wsTestsExec } from "../_utils";
 
 describe("cloudflare-durable", () => {
-  wsTestsExec("wrangler dev -c ./wrangler-durable.toml --port $PORT");
+  wsTestsExec(
+    "wrangler dev -c ./wrangler-durable.toml --inspector-port 0 --port $PORT",
+  );
 });
--- test/adapters/cloudflare.test.ts
+++ test/adapters/cloudflare.test.ts
@@ -2,5 +2,8 @@ import { describe } from "vitest";
 import { wsTestsExec } from "../_utils";
 
 describe("cloudflare", () => {
-  wsTestsExec("wrangler dev -c ./wrangler.toml --port $PORT", false);
+  wsTestsExec(
+    "wrangler dev -c ./wrangler.toml --inspector-port 0 --port $PORT",
+    false,
+  );
 });
