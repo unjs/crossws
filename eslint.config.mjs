d3ab5f8d562d121b53425b56e0a80c6c6e6d7fa5
Pooya Parsa <pooya@pi0.io>
2024-10-04 15:12:07 +0200

    chore: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,14 +2,11 @@ import unjs from "eslint-config-unjs";
 
 // https://github.com/unjs/eslint-config
 export default unjs({
-  ignores: [
-  "types",
-  "**/.wrangler",
-  "**/.docs/**",
-],
+  ignores: ["types", "**/.wrangler", "**/.docs/**"],
   rules: {
-  "@typescript-eslint/no-unused-vars": 0,
-  "no-useless-constructor": 0,
-  "unicorn/consistent-function-scoping": 0
-},
+    "@typescript-eslint/no-unused-vars": 0,
+    "no-useless-constructor": 0,
+    "unicorn/consistent-function-scoping": 0,
+    "@typescript-eslint/no-empty-object-type": 0,
+  },
 });
